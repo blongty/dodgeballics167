8ff3b4e378624d937f690ff9ec00f26ef8e449ba
wizardkingrabbit <jingtil2@uci.edu>
2020-03-16 02:46:17 +0800

    main scene merge
    for unknown reason it cannot shake

--- components/objects/Player/shake/shake.gd
+++ components/objects/Player/shake/shake.gd
@@ -31,6 +31,3 @@ func _on_dur_timeout():
 func reset():
 	$ShakeTween.interpolate_property(camera, "offset", camera.offset, Vector2(), $fre.wait_time, TRANS, EASE)
 	$ShakeTween.start()
-func _on_Field_ready():
-	pass;
-	#start();
--- levels/level/test.gd
+++ levels/level/test.gd
@@ -2,6 +2,7 @@ extends Node2D
 var camera
 func _ready():
 	camera = get_node("Camera2D")
+	camera.shake()
 	
 func shake():
 	camera.shake()
